c900ace7e3abfe7f11fb47441af4e200e0cf1864
eslam-123 <84737908+eslam-123@users.noreply.github.com>
2021-05-24 20:34:11 +0200

    Create OPPO.java

---
+++ Mobilephone/OPPO.java
@@ -0,0 +1,17 @@
+package javaapplication26;
+
+/**
+ *
+ * @author Copy Center
+ */
+public class OPPO extends MobilePhone {
+    
+    int size;
+    
+    public OPPO(){}
+
+    public OPPO(int CPU, int size)
+    {
+    
+    }
+}
